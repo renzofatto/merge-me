956b97dbcedc1afb5879154012f9ed8b2b9891e0
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-09-01 01:00:35 -0300

    Changed full name to be always uppercase

--- MergeMe/MergeMe/User.cs
+++ MergeMe/MergeMe/User.cs
@@ -12,7 +12,7 @@ namespace MergeMe
 
         public string LastName { get; set; }
 
-        public string FullName { get => Name + " " + LastName; } 
+        public string FullName { get => Name.ToUpper() + " " + LastName.ToUpper(); } 
 
     }
 }
