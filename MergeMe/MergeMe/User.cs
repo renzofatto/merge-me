4af88a9a4cf5a2490220b3085ffac87e4769376d
Facundo Arancet Iza <facuarancet97@gmail.com>
2021-09-01 00:46:00 -0300

    Created User class

---
+++ MergeMe/MergeMe/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeMe
+{
+    class User
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get => Name + " " + LastName; } 
+
+    }
+}
